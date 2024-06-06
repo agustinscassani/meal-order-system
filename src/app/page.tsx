5f33a236162199839887edb795f29017ac1d2a61
Agustín Cassani <agustinscassani@gmail.com>
2024-06-06 15:15:14 -0300

    MongoDB and Prisma client added.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import Dashboard from "@/components/dashboard"
 import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
-export default function Home() {
+import type { meal } from '@prisma/client';
+import db from "@/db";
+
+export default async function Home() {
+  const meals = await db.meal.findMany();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Dashboard />
       <LoginLink>Sign in</LoginLink>
       <RegisterLink>Sign up</RegisterLink>
+      {meals.map((meal: meal) => <span key={meal.id}>{meal.name}</span>)}
     </main>
   )
 }
